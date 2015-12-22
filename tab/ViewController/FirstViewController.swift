8bcbe7a6c5e67fca875f0abe683e24e89eb2f728
KentarOu <kanno@kentarou.me>
2015-12-23 04:48:32 +0900

    add Navigation

---
+++ tab/ViewController/FirstViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FirstViewController.swift
+//  tab
+//
+//  Created by KentarOu on 2015/12/23.
+//  Copyright © 2015年 KentarOu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ tab/ViewController/SecondViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecondViewController.swift
+//  tab
+//
+//  Created by KentarOu on 2015/12/23.
+//  Copyright © 2015年 KentarOu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
